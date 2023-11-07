bd8f4d33d5ec9bb258b076e2f5e156812eaf7020
Will Weidler <98359135+wiidler@users.noreply.github.com>
2023-11-06 22:11:57 -0600

    notation added and formatted for unix

--- fibo.py
+++ fibo.py
@@ -2,8 +2,12 @@
 # Author: Will Weidler
 # Date: 11/2/23
 
+# Constant for fibonacci matrix calculation
 ArrayP = [[0, 1], [1, 1]]
 
+
+# Function: Fibonacci sequence
+# Returns: Recursive call of fibo function
 def fibo(n, count=None):
     if count is None:
         count = [0] * (n + 1)
@@ -16,6 +20,8 @@ def fibo(n, count=None):
     return fibo(n - 1, count) + fibo(n - 2, count)
 
 
+# Function: Naive matrix multiplication
+# Returns: result of multiplication and count of total multiplications
 def naive_matrix_mult(P, Q, count):
     a, b = len(P), len(P[0])
     g, h = a, b
@@ -29,6 +35,9 @@ def naive_matrix_mult(P, Q, count):
 
     return result, count
 
+
+# Function: Matrix power
+# Returns: result of exponent and count of total multiplications
 def matrix_power(P, num):
     if num == 1:
         return P, 0
@@ -41,15 +50,21 @@ def matrix_power(P, num):
     return result, count
 
 
-
 def main():
+    # prompt for fibonacci input
     FiboNum = int(input())
+    # initialize the count
     count = [0] * (FiboNum + 1)
+    # recieves count from fibo function
     result = fibo(FiboNum, count)
+    # prints fibbonacci number
     print(count[1])
+    # prints n+1 lines presenting how many times each count of n was called
     for i in range(FiboNum + 1):
         print(f"fibo({i}) : {count[i]}")
+    # recieves the cound from the second method
     result, count = matrix_power(ArrayP, FiboNum)
+    # prints the number of multiplications
     print(count)
 
 
